42e6e46994d0bd5c91c74ca02b7bfcfd53ae7d5f
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-18 13:42:06 -1000

    first commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express' ; // common JS import syntax
 import chalk from 'chalk'; // enabling easy backend syntax with type: module in package.json
 import dotenv from 'dotenv'; // .ENV PACKAGE
 import cors from 'cors';
-import { Server } from 'socket.io'; // rename with curly braces in {}
+import { Server as SocketServer } from 'socket.io'; // rename with curly braces in {}
 
 dotenv.config();
 
@@ -16,4 +16,4 @@ const httpServer = app.listen(port, () =>
 console.log(chalk.cyan(`Listening on PORT: ${port}`)));
 
 // io is our socket server
-const io =
+const io = new SocketServer(httpServer)// this is a class taking in a server as a variable!
