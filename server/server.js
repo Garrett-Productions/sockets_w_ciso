20fab7d10ecca33c58a3f20cbf4012b99dc80299
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-18 13:36:14 -1000

    first commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 import express from 'express' ; // common JS import syntax
 import chalk from 'chalk'; // enabling easy backend syntax with type: module
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; // .ENV PACKAGE
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
+// const print = console.log
