fbbae266bad0e2766568a194197b564404bfc24c
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-18 13:33:55 -1000

    first commit

--- server/server.js
+++ server/server.js
@@ -1,1 +1,4 @@
-const express = require('express'); // common JS import syntax
+import express from 'express' ; // common JS import syntax
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+                        // enabling easy backend syntax with type: module 
